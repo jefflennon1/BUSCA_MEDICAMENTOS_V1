d3ca01a28526f7249e543bddbae8e063dcc33e9d
jeff lennon <jeffersonlennon1@outlook.com>
2021-01-08 16:33:44 -0300

    Mais um pouco do layout

--- src/components/listaDeMedicamentos/index.js
+++ src/components/listaDeMedicamentos/index.js
@@ -2,40 +2,68 @@ import React,{ useEffect, useState } from 'react';
 // import Api from '../../../services/Api';
 import Api from '../../../services/Api';
 import './style.css';
-import Icon from '../../assets/icon_medico2.png'
+import icon from '../../assets/icon_medico2.png';
+import search from '../../assets/search.png'
 
 
 export default function ListaDeMedicamentos(){
-    const [ medicamento, setMedicamento ] = useState([]);
-    function buscaComposto(){
-        const value = document.getElementById('inp_bus_comp').value;
-         value == "" ? setMedicamento([]) :
+   
+    useEffect(()=>{
+        document.getElementById('lista_medicamentos').style.display = 'none';
+    }, []);
 
-         Api.get(`/composicao?COMPOSICAO=${value}`).then((response)=>{
-            setMedicamento(response.data);
-        });
+    const [ medicamento, setMedicamento ] = useState([]);
+
+
+    function buscaComposto(){
+            document.getElementById('search').style.display = 'none';
+            const value = document.getElementById('inp_bus_comp').value;
+            const prop = document.getElementById('select_tipo').value;
+            console.log(prop)
+        
+            if(value == ""){ 
+                setMedicamento([]);
+                document.getElementById('search').style.display = 'inline';
+                document.getElementById('lista_medicamentos').style.display = 'none';
+                return;
+                }
+        
+            Api.get(`/composicao?${prop}=${value}`).then((response)=>{
+                setMedicamento(response.data);
+                document.getElementById('lista_medicamentos').style.display = 'inline';
+            });
+
     }
-  
    
     return(
         <>
            <section id="container"> 
                 <section className="principal">
-                    <img src={Icon} alt=""/>
+                    <img src={icon} alt=""/>
                         <h1>Olá, tudo bem com você?</h1>
-                        <input type="text" placeholder="Digite o nome do composto" 
-                        autoComplete="on" id="inp_bus_comp"
-                        onChange={()=>buscaComposto()}
-                        />
-                        <div className="lista_medicamentos">
-                                {medicamento.map( medicamento=> <div key={medicamento._id}>
-
-                                    <div className="nomeMedicamento">
-                                        <span> {medicamento.NOME}</span>
-                                    </div>   
-                                </div>
-                                )}  
-                        </div> 
+                            <p>
+                                Já passou por alguma situação em que você está prestes a tomar um medicamento
+                                mas tem receio pois você não sabe do que ele é composto e isso pode 
+                                acabar em uma reação alérgica?
+                                Aqui você pode buscar a composição de qualquer medicamento!
+                            </p>
+                                <select name="select_tipo" id="select_tipo" >
+                                    <option value="COMPOSICAO">COMPOSIÇÃO</option>
+                                    <option value="NOME">MEDICAMENTO</option>
+                                </select>
+                            <span id="inpuEicon">
+                                <input type="text" placeholder="Digite o nome do composto" 
+                                    autoComplete="on" id="inp_bus_comp" onChange={()=>buscaComposto()}/>
+                                <img src={search} alt="search" id="search"/>
+                            </span>   
+                            <div className="lista_medicamentos" id="lista_medicamentos">
+                                    {medicamento.map( medicamento=> <div key={medicamento._id}>
+                                        <div className="nomeMedicamento"  onClick={()=>buscaBulaDomedicamento(medicamento.NOME)}>
+                                            <span> {medicamento.NOME}</span>
+                                        </div>   
+                                    </div>
+                                    )}  
+                            </div> 
                 </section> 
             </section>         
         </>
@@ -45,20 +73,3 @@ export default function ListaDeMedicamentos(){
 
 
 
-async function buscaDadosDaApiAbcFarma(){
-    var form = new FormData();
-    form.append('cnpj_cpf', '03332548000127');
-    form.append('senha', 'aluisiovpn');
-    form.append('cnpj_sh', '07693076000199');
-    form.append('pagina', '3');
-
-    const response = await axios({
-        method: 'post',
-            url: 'https://webserviceabcfarma.org.br/webservice/',
-            data: form,
-            headers: {
-                'Content-Type': `multipart/form-data; boundary=${form._boundary}`,
-            },
-    })
-        setMedicamento(response.data.data);
-}
